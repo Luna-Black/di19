6babdecaf3838e5474465cce4cd88e1393e1e494
Xavier <xavier.chemin@viacesi.fr>
2020-02-04 17:00:28 +0100

    add Category.php -> bdd add and bdd delete

--- src/Model/Category.php
+++ src/Model/Category.php
@@ -3,13 +3,31 @@ namespace src\Model;
 
 class Category {
     private $Id;
-    private $Name;
+    private $Nom;
 
-    public function SqlAdd() {
+    public function SqlAdd(\PDO $bdd) {
+        try {
+            $requete = $bdd->prepare('INSERT INTO categories (Nom) VALUES(:Nom)');
+            $requete->execute([
+                "Nom"=> $this->getNom(),
+            ]);
+            return array("result"=>true,"message"=>$bdd->lastInsertId());
+        }catch (\Exception $e){
+            return array("result"=>false,"message"=>$e->getMessage());
+        }
 
     }
 
-    public function SqlDelete() {
+    public function SqlDelete(\PDO $bdd,$idCategory) {
+        try {
+            $requete = $bdd->prepare('DELETE FROM categories where Id = :idCategory');
+            $requete ->execute([
+                'idCategory'=>$idCategory
+            ]);
+            return true;
+        }catch (\Exception $e){
+            return false;
+        }
 
     }
 
