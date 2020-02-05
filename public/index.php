2d1fc7ac2be132f1b2ec9ffcfa0b768fc8df6a94
Caro <cfouquet.web@gmail.com>
2020-02-05 09:57:06 +0100

    i commit for to pull xD

--- public/index.php
+++ public/index.php
@@ -37,6 +37,7 @@ $router->post('/Contact/sendMail', 'Contact#sendMail');
 $router->get('/Login', 'User#loginForm');
 $router->post('/Login', 'User#loginCheck');
 $router->get('/Logout', 'User#logout');
+$router->get('/Signup', 'User#signup'); //
 
 echo $router->run();
 
