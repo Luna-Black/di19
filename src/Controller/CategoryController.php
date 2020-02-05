70e6e46775bf1766dac3f84ef14d682e4ccb56a0
Xavier <xavier.chemin@viacesi.fr>
2020-02-05 10:13:55 +0100

    wip

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,20 @@ use src\Model\Bdd;
 class CategoryController extends AbstractController {
 
     public function add() {
-        UserController::roleNeed('')
+        UserController::roleNeed('Administrateur');
+        if($_POST){
+            $category = new Category();
+            $category->setNom($_POST['Nom']);
+
+            $category->SqlAdd(Bdd::GetInstance());
+            header('Location:/Category');
+        }
 
     }
 
-    public function delete() {
+    public function delete($categoryID) {
+        $categorySQL = new Category();
+        //$category = $categorySQL->
 
     }
 
